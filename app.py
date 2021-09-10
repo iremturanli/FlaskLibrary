1762e83433cd22c439571ac9770f9a273afa1188
= <iremturanli@hotmail.com>
2021-09-10 17:16:41 +0300

    lv

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask,render_template,url_for,redirect
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask import request
+from db.library import Library, books,logrecords
+import hashlib as hasher
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/turkai/Masaüstü/workplace/Flask/library/flasklibrarymodify/db/library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False 
+
+
+db=SQLAlchemy(app)
+sifreleyici = hasher.sha224()
+
+
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    try:
+        if request.method == 'POST':
+
+            isim=request.form.get('name')
+            password=request.form.get('passw')
+            sifreleyici.update(password.encode())
+            cikti=sifreleyici.hexdigest()
+           
+            
+            print(cikti)
+         
+            
+            uye=Uye.query.filter_by(uye_kullaniciadi=isim,uye_sifre=cikti).first()
+
+            if uye.uye_kullaniciadi==isim and uye.uye_sifre==cikti:
+                return redirect(url_for('add'))
+
+            else:
+                mesaj="Hatalı giriş!,Tekrar deneyiniz."
+                return render_template("index.html",mesaj=mesaj)            
+        else:  
+                 mesaj="Hoşgeldiniz."
+                 return render_template("index.html",mesaj=mesaj)
+    except:
+        mesaj="Hatalı giriş,Tekrar deneyiniz."
+        return render_template("index.html",mesaj=mesaj)
+
+        
+@app.route("/add",methods=["GET"]) 
+def add():
+    
+    uyeler=Uye.query.all()
+  
+    return render_template("add.html",books=books,uye=uyeler)
+
+
+@app.route("/log",methods=["GET"])
+def log():
+    return render_template("log.html",logrecords=logrecords)
+
+  
+@app.route("/access",methods=["GET","POST"])
+def access():
+    
+    try:  
+        if request.method == 'POST':
+        
+            isim=request.form.get('name')
+            sifre=request.form.get('psw')
+            print(access)
+            uye=Uye.query.filter_by(uye_kullaniciadi=isim,uye_sifre=sifre).first()
+            if uye.uye_kullaniciadi==isim and uye.uye_sifre==sifre and uye.uye_durum==True:
+                return redirect(url_for('log'))
+            else:
+                mesaj="Bu sayfaya erişiminiz bulunmamaktadır."
+                return render_template("access.html",mesaj=mesaj)
+        else:
+            mesaj="Yönetici sayfasına hoşgeldiniz."
+            return render_template("access.html",mesaj=mesaj)
+              
+
+    except:    
+         mesaj="Hatalı giriş,tekrar deneyiniz."
+         return render_template('access.html',mesaj=mesaj)
+
+
+
+
+class Uye(db.Model):
+
+    uye_id=db.Column(db.Integer,primary_key=True)
+    uye_kullaniciadi=db.Column(db.String,nullable=False)
+    uye_sifre=db.Column(db.String,nullable=False)
+    uye_durum=db.Column(db.Boolean,nullable=False)
+
+    def __init__(self,uye_kullaniciadi,uye_sifre,uye_durum):
+
+        self.uye_kullaniciadi=uye_kullaniciadi
+        self.uye_sifre=uye_sifre
+        self.uye_durum=uye_durum
+
+    def addClass(self):
+        db.session.add(self)
+        db.session.commit()
+
+db.create_all()
+
+   
+# iremt=Uye(uye_kullaniciadi="iremt",uye_sifre="1234567890",uye_durum=1)
+# print(iremt.uye_sifre)
+# sifreleyici.update(iremt.uye_sifre.encode())
+# cikti=sifreleyici.hexdigest()
+# iremt.uye_sifre=cikti
+# iremt.addClass()
+   
+# user=Uye(uye_kullaniciadi="user",uye_sifre="12345",uye_durum=0)
+# sifreleyici.update(user.uye_sifre.encode())
+# cikti=sifreleyici.hexdigest()
+# user.uye_sifre=cikti
+# user.addClass()
+   
+
+# members=Uye.query.all()
+# for i in range(len(members)):
+#     sifreler=members[i].uye_sifre
+#     sifreleyici.update(sifreler.encode())
+#     cikti=sifreleyici.hexdigest()
+#     print(cikti)
+#     members[i].uye_sifre=cikti
+#     db.session.commit()
+ 
+   
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    # app.run(host='192.168.1.97',debug=True, port=5000)
---
+++ db/library.py
@@ -0,0 +1,101 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+import datetime
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/turkai/Masaüstü/workplace/Flask/library/flasklibrary/db/library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False 
+
+db=SQLAlchemy(app)
+
+
+class Library(db.Model):
+
+    BookID=db.Column(db.Integer,primary_key=True)
+    BookName=db.Column(db.String,nullable=False)
+    Yearofpublication=db.Column(db.String,nullable=False)
+    AuthorName=db.Column(db.String,nullable=False)
+    Category=db.Column(db.String,nullable=False)
+    Addp=db.Column(db.String,nullable=False)
+    Book=db.relationship("Log",backref='library')
+
+    def __init__(self,BookName,Yearofpublication,AuthorName,Category,Addp):
+        self.BookName=BookName
+        self.Yearofpublication=Yearofpublication
+        self.AuthorName=AuthorName
+        self.Category=Category
+        self.Addp=Addp
+
+
+    def addClass(self):
+
+        # l1=Library(BookName,Yearofpublication,AuthorName,Category,Addp)
+        db.session.add(self)
+        db.session.commit()
+
+
+class Log(db.Model):
+
+    BookID_log=db.Column(db.Integer,primary_key=True)
+    Date=db.Column(db.String,nullable=False)
+    Library_id=db.Column(db.Integer,db.ForeignKey('library.BookID'),nullable=False)
+    Info=db.Column(db.String)
+    Name=db.Column(db.String)
+    OldVersion=db.Column(db.String)
+    NewVersion=db.Column(db.String)
+
+    def __init__(self,Library_id,Info,Name,OldVersion,NewVersion):
+        self.Date=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.Library_id=Library_id
+        self.Info=Info
+        self.Name=Name
+        self.OldVersion=OldVersion
+        self.NewVersion=NewVersion
+
+    def addClass(self):
+
+        # l1=Library(BookName,Yearofpublication,AuthorName,Category,Addp)
+        db.session.add(self)
+        db.session.commit()
+
+
+    # def addClass(Library_id,Info,Name,OldVersion,NewVersion):
+    
+    #     lg1=Log(Library_id,Info,Name,OldVersion,NewVersion)
+    #     db.session.add(lg1)
+    #     db.session.commit()
+
+
+db.create_all()
+
+books=Library.query.all()
+logrecords=Log.query.all()
+
+
+
+# l1=Library(BookName="Küçük Prens",Yearofpublication=2000,AuthorName="Ali",Category="Dram",Addp="İrem")
+# l1.addClass()
+# Log.addClass(Library_id=l1.BookID,Info='Book added',Name=l1.Addp,OldVersion=None,NewVersion=l1.BookName)
+
+def add(BookName,Yearofpublication,AuthorName,Category,Addp):
+    l1=Library(BookName,Yearofpublication,AuthorName,Category,Addp)
+    l1.addClass()
+    lg=Log(Library_id=l1.BookID,Info='Book added',Name=Addp,OldVersion=None,NewVersion=BookName)
+    lg.addClass()
+
+
+
+def delete(key):
+    Person=input("Name: ")
+    db.session.query(Library).filter(Library.BookID==key).delete()
+    lg=Log(Library_id=key,Info='Book Deleted',Name=Person,OldVersion=None,NewVersion=None)
+    lg.addClass()
+
+    db.session.commit()
+
+# add("Casper",1778,"John","Korku","İrem")
+# # add("Sefiller",2000,"Victor Hugo","Dram","Berk")
+# add("1asd",2015,"Selena","Political","Fatma")
+# add("1asd",1990,"Mona","Bilim","Sena")
+# delete(4)
